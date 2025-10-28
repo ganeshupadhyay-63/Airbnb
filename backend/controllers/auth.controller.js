73a1faf77293aeb22c628c4a07775f331e8d158c
Ganesh Upadhyay <ganeshupadhyay729@gmail.com>
2025-10-28 22:00:39 +0530

    Update auth Controller.js

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -16,7 +16,7 @@ export const signUp = async(req, res)=>{
         res.cookie("token",token,{
             httpOnly: true,
             secure:true,
-            sameSite: "null",
+            sameSite: "none",
             maxAge: 7*24*60*60*1000
         })
 
@@ -46,7 +46,7 @@ export const login = async (req,res)=>{
         res.cookie("token",token,{
             httpOnly: true,
             secure:true",
-            sameSite: "null",
+            sameSite: "none",
             maxAge: 7*24*60*60*1000
         })
 
