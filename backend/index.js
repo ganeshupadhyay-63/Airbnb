01d41cb29418f1ae0d6ba89e74902afbaecb203b
Ganesh Upadhyay <ganeshupadhyay729@gmail.com>
2025-10-28 22:45:04 +0530

    Update CORS origin for production deployment

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser()); 
 
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://airbnb-front-ro3g.onrender.com",
   credentials: true
 }));
 
